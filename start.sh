8093c7ed36775df4ffd2879ac2cac52f00b95841
ucr_lixian <593771171@qq.com>
2022-01-07 17:17:16 +0800

    feat:jenkins启动脚本加注释

--- start.sh
+++ start.sh
@@ -1,14 +1,18 @@
 #拉取代码
 git pull
+echo "拉取代码成功"
 
 #安装依赖和打包
 npm install && npm run build
+echo "安装依赖和打包成功"
 
 # 删除原有容器
 docker rm -f demo &> /dev/null
+echo "删除原有容器成功"
 
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 8080:80 \
     -v $PWD/dist:/usr/share/nginx/html \
     --name demo nginx
+echo "构建容器成功"
