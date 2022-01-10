dd0303a263b6d62712172fe1679249bba104b76d
ucr_lixian <593771171@qq.com>
2022-01-10 15:07:33 +0800

    feat:自动化部署测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ msg2 }}</h1>
+    <h1>{{ msg2 }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +37,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      msg2:'cicd自动化部署测试'
+    }
   }
 }
 </script>
